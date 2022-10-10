99df4ca0f0a2f4c07002dcace3ca595a18a89805
droneinspection <abhishek2aa2@gmail.com>
2022-10-10 14:46:51 +0300

    Signin Signup Layout done
    --

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
-import { ChannelContainer, ChannelListContainer } from './components'
+import { ChannelContainer, ChannelListContainer, Auth } from './components'
 
 import './App.css'
 
@@ -11,7 +11,11 @@ const apiKey = 'xx38euy6udc4'
 
 const client = StreamChat.getInstance(apiKey)
 
+const authToken = false
+
 const App = () => {
+  if (!authToken) return <Auth />
+  
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme='team light'>
