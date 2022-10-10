e1e7a8df1d9d0c26b414a5eaa2837fef1b49a73e
droneinspection <abhishek2aa2@gmail.com>
2022-10-10 13:19:26 +0300

    round 2: basic structure
    had to delete the whole repo and start again since i messed up something along the way ://

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+import { ChannelContainer, ChannelListContainer } from './components'
+
+import './App.css'
+
+const apiKey = 'xx38euy6udc4'
+
+const client = StreamChat.getInstance(apiKey)
+
+const App = () => {
+  return (
+    <div>
+        <div className='app__wrapper'>
+            <Chat client={client} theme='team light'>
+                <ChannelListContainer 
+                
+                />
+                <ChannelContainer 
+                
+                />
+            </Chat>
+        </div>
+    </div>
+  )
+}
+
+export default App
