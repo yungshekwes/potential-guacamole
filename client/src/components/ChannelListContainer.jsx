1b0d8d50ba03ae7f9065402bfe30236aa4607759
droneinspection <abhishek2aa2@gmail.com>
2022-10-17 00:23:03 +0300

    prod ready
    --

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,7 +3,7 @@ import { ChannelList, useChatContext } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
-import HospitalIcon from '../assets/hospital.png'
+import AvoIcon from '../assets/avocado.png'
 import LogoutIcon from '../assets/logout.png'
 
 const cookies = new Cookies()
@@ -12,7 +12,7 @@ const SideBar = ({logout}) => (
     <div className='channel-list__sidebar'>
         <div className='channel-list__sidebar__icon1'>
             <div className='icon1__inner'>
-                <img src={HospitalIcon} alt='AvoChat' width='30' />
+                <img src={AvoIcon} alt='AvoChat' width='30' />
             </div>
         </div>
         <div className='channel-list__sidebar__icon2'>
